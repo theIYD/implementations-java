5f2a743aee660c485a8426991b4abec0fde5c52e
Idrees <dargahidrees@gmail.com>
2017-01-22 19:22:29 +0530

    Calculator: chooseOperations()
    Use this method to accept the entry and go to the appropriate method
    
    Signed-off by: Idrees Dargahwala

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -13,6 +13,7 @@ public class Calculator {
 
 		Calculator calc = new Calculator();
 		calc.acceptName();
+		calc.chooseOperations();
 	}
 
 	//Accept user's name
@@ -27,5 +28,44 @@ public class Calculator {
 		System.out.println("\t\t\t Welcome "+username+"\t\t\t");
 		System.out.println();
 	}
-	
+
+	//Accept the entry and go to the appropriate methods
+	public void chooseOperations() {
+
+		boolean validEntry = false;
+
+		do{
+			System.out.print(" 1. Addition\n 2.Subtraction\n 3. Multiplication\n 4.Division\n");
+			System.out.println();
+			System.out.println("Which operation you want to choose ?");
+
+			int selectOperator = sc.nextInt();
+
+			switch(selectOperator) {
+
+				case 1: //additionOperation();
+						validEntry = true;
+						break;
+
+				case 2: //subtractionOperation();
+						validEntry = true;
+						break;
+
+				case 3: //multiplicationOperation();
+						validEntry = true;
+						break;
+
+				case 4: //divisionOperation();
+						validEntry = true;
+						break;
+
+				default: System.out.println("Try again !");
+						 validEntry = false;
+						 break;
+			}
+		}while(validEntry==false);
+
+
+	}
+
 }
